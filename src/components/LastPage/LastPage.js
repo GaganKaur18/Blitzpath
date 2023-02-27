6c8e17e80419f491bf9c24c4cca7cfab16148db9
Gagan_kaur <76256435+GaganKaur18@users.noreply.github.com>
2023-02-27 09:44:50 +0530

    Update LastPage.js

--- src/components/LastPage/LastPage.js
+++ src/components/LastPage/LastPage.js
@@ -10,111 +10,138 @@ import styles from "./LastPage.module.css";
 
 import { EffectCoverflow, Navigation, Pagination } from "swiper";
 import { swipeMove } from "react-slick/lib/utils/innerSliderUtils";
-const LastPage = () => {
+const LastPage = ({ width }) => {
   return (
-    <>    <div className="heading" style={{position:'absolute', left:'43%'}}><h1>Customer Stories</h1></div>
-    <div className={styles.containerstories}>
-    
-    <div className={styles.container}>
-      <Swiper
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        loop={true}
-        slidesPerView={"4"}
-        coverflowEffect={{
-          rotate: 30,
-          stretch: 0,
-          depth: 50,
-          modifier: 1,
-          slideShadows: true,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Navigation, EffectCoverflow, Pagination]}
-        className={styles.swiper}
-      >
-        <SwiperSlide className={styles.sliderElement}>
-          <div>
-            <div
-              style={{
-                background:
-                  "url(https://swiperjs.com/demos/images/nature-1.jpg)",
-              }}
-            ></div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className={styles.sliderElement}>
-          <div>
-            <div
-              style={{
-                background:
-                  "url(https://swiperjs.com/demos/images/nature-2.jpg)",
-              }}
-            ></div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className={styles.sliderElement}>
-          <div>
-            <div
-              style={{
-                background:
-                  "url(https://swiperjs.com/demos/images/nature-3.jpg)",
-              }}
-            >
-              heyy
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className={styles.sliderElement}>
-          <div
-            style={{
-              background: "url(https://swiperjs.com/demos/images/nature-4.jpg)",
+    <>
+      {" "}
+      <div className={styles.heading}>
+        <h1>Customer Stories</h1>
+      </div>
+      <div className={styles.containerstories}>
+        <div className={styles.container}>
+          <Swiper
+            effect={"coverflow"}
+            grabCursor={true}
+            centeredSlides={true}
+            loop={true}
+            slidesPerView={`${width > 768 ? "4" : "3"}`}
+            coverflowEffect={{
+              rotate: 30,
+              stretch: 0,
+              depth: 50,
+              modifier: 1,
+              slideShadows: true,
             }}
-          ></div>
-        </SwiperSlide>
-        <SwiperSlide className={styles.sliderElement}>
-          <div
-            style={{
-              background: "url(https://swiperjs.com/demos/images/nature-5.jpg)",
+            pagination={{
+              clickable: true,
             }}
-          ></div>
-        </SwiperSlide>
-        <SwiperSlide className={styles.sliderElement}>
-          <div
-            style={{
-              background: "url(https://swiperjs.com/demos/images/nature-6.jpg)",
-            }}
-          ></div>
-        </SwiperSlide>
-        <SwiperSlide className={styles.sliderElement}>
-          <div
-            style={{
-              background: "url(https://swiperjs.com/demos/images/nature-7.jpg)",
-            }}
-          ></div>
-        </SwiperSlide>
-        <SwiperSlide className={styles.sliderElement}>
-          <div
-            style={{
-              background: "url(https://swiperjs.com/demos/images/nature-8.jpg)",
-            }}
-          ></div>
-        </SwiperSlide>
-        <SwiperSlide className={styles.sliderElement}>
-          <div
-            style={{
-              background: "url(https://swiperjs.com/demos/images/nature-9.jpg)",
-            }}
-          ></div>
-        </SwiperSlide>
-      </Swiper>
-    </div>
-    </div>
+            navigation={true}
+            modules={[Navigation, EffectCoverflow, Pagination]}
+            className={styles.swiper}
+          >
+            <SwiperSlide className={styles.sliderElement}>
+              <div>
+                <div
+                  className={styles.sliderElementDiv}
+                  style={{
+                    background:
+                      "url(https://swiperjs.com/demos/images/nature-1.jpg)",
+                  }}
+                ></div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={styles.sliderElement}>
+              <div>
+                <div
+                  className={styles.sliderElementDiv}
+                  style={{
+                    background:
+                      "url(https://swiperjs.com/demos/images/nature-2.jpg)",
+                  }}
+                ></div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={styles.sliderElement}>
+              <div>
+                <div
+                  className={styles.sliderElementDiv}
+                  style={{
+                    background:
+                      "url(https://swiperjs.com/demos/images/nature-3.jpg)",
+                  }}
+                ></div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={styles.sliderElement}>
+              <div>
+                <div
+                  className={styles.sliderElementDiv}
+                  style={{
+                    background:
+                      "url(https://swiperjs.com/demos/images/nature-4.jpg)",
+                  }}
+                ></div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={styles.sliderElement}>
+              <div>
+                <div
+                  className={styles.sliderElementDiv}
+                  style={{
+                    background:
+                      "url(https://swiperjs.com/demos/images/nature-5.jpg)",
+                  }}
+                ></div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={styles.sliderElement}>
+              <div>
+                <div
+                  className={styles.sliderElementDiv}
+                  style={{
+                    background:
+                      "url(https://swiperjs.com/demos/images/nature-6.jpg)",
+                  }}
+                ></div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={styles.sliderElement}>
+              <div>
+                <div
+                  className={styles.sliderElementDiv}
+                  style={{
+                    background:
+                      "url(https://swiperjs.com/demos/images/nature-7.jpg)",
+                  }}
+                ></div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={styles.sliderElement}>
+              <div>
+                <div
+                  className={styles.sliderElementDiv}
+                  style={{
+                    background:
+                      "url(https://swiperjs.com/demos/images/nature-8.jpg)",
+                  }}
+                ></div>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className={styles.sliderElement}>
+              <div>
+                <div
+                  className={styles.sliderElementDiv}
+                  style={{
+                    background:
+                      "url(https://swiperjs.com/demos/images/nature-9.jpg)",
+                  }}
+                ></div>
+              </div>
+            </SwiperSlide>
+          </Swiper>
+        </div>
+      </div>
     </>
-
   );
 };
 
